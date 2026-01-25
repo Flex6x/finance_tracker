ef498937862569b0285cb22fb7c2d2a7d920a1ae
Flex6x <hanf.felix@outlook.de>
2026-01-25 16:51:38 +0100

    ausgaben können jetzt in tab2 eingetragen werden, wird noch nicht direkt aktualisiert

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
 from tkinter import ttk
 import json
+import os
 
 # Auswahl zwischen dark/light und theme
 ctk.set_appearance_mode("dark")
@@ -9,7 +10,7 @@ ctk.set_default_color_theme("blue")
 # main window
 app = ctk.CTk()
 app.title("Finanz Tracker")
-app.geometry("800x600")
+app.geometry("1040x800")
 app.resizable(width=True, height =True) #Fenstereinstellungen skalierbar
 
 # Rows and Columns in app
@@ -48,7 +49,8 @@ tab1.grid_columnconfigure(0, weight=1)
 
 # rows and columns in tab2
 tab2.grid_rowconfigure(0, weight=1)
-tab2.grid_columnconfigure(0, weight=1)
+tab2.grid_rowconfigure(1, weight=3)
+tab2.grid_columnconfigure((0,1,2,3), weight=1)
 
 # rows and columns in tab3
 tab3.grid_rowconfigure(0, weight=1)
@@ -77,13 +79,76 @@ label = ctk.CTkLabel(
 )
 label.grid(row=0, column=0,sticky ="ew")
 
+# Funktionen für die Entrys
+produkt=""
+preis=""
+datum=""
+def answer_produkt():
+    global produkt
+    produkt = entry_produkt.get()
+def answer_preis():
+    global preis
+    preis = entry_preis.get()
+def answer_datum():
+    global datum
+    datum = entry_datum.get()
+
+def save_ausgabe():
+    neu = {
+    "produkt": produkt,
+    "preis": preis,
+    "datum": datum
+}
+
+    # Prüfung, ob .json und array existiert
+
+    if os.path.exists("ausgaben.json"):
+        with open("ausgaben.json", "r", encoding="utf-8") as datei:
+            try:
+                daten = json.load(datei)
+                # falls ein einzelnes Objekt drinsteht, in Liste umwandeln
+                if isinstance(daten, dict):
+                    daten = [daten]
+            except json.JSONDecodeError:
+                daten = []
+    else:
+        daten = []
+
+    daten.append(neu) # Eintrag wird der zu "daten hinzugefügt"
+
+    # Anhang wird ausgeführt
+    with open("ausgaben.json", "w", encoding="utf-8") as datei:
+        json.dump(daten, datei, indent=4, ensure_ascii=False)
+
+
+# Entry zu Produkt
+entry_produkt = ttk.Entry(tab2)
+entry_produkt.grid (row =0, column=0,sticky="w")
+button_produkt=ttk.Button(tab2,text="<<-- Produkt", command =answer_produkt)
+button_produkt.grid(row=0, column=0,sticky="e")
+
+# Entry zu Preis
+entry_preis = ttk.Entry(tab2)
+entry_preis.grid (row =0, column=1,sticky="w")
+button_preis=ttk.Button(tab2,text="<<-- Preis in €", command =answer_preis)
+button_preis.grid(row=0, column=1,sticky="e")
+
+# Entry zu Datum
+entry_datum = ttk.Entry(tab2)
+entry_datum.grid (row =0, column=2,sticky="w")
+button_datum=ttk.Button(tab2,text="<<-- Datum in YYYYMMDD", command =answer_datum)
+button_datum.grid(row=0, column=2,sticky="e")
+
+button_save_ausgabe=ttk.Button(tab2,text="start",command =save_ausgabe)
+button_save_ausgabe.grid(row=0, column=3,sticky="ew")
+
 # Tabelle in Tab 2 anlegen
 
 table = ttk.Treeview(tab2,columns = ("produkt", "preis", "datum"),show="headings")
 table.heading("produkt",text="Produkt")
-table.heading("preis",text="Preis")
-table.heading("datum",text="Datum")
-table.grid(row=0,column=0,sticky="nsew")
+table.heading("preis",text="Preis in €")
+table.heading("datum",text="Datum in YYYYMMDD")
+table.grid(row=1,column=0,columnspan=4,sticky="nsew")
 
 # Einträge aus ausgaben.json lesen und in listen eintragen 
 
@@ -91,19 +156,19 @@ with open("ausgaben.json", "r") as fh:
     ausgaben = json.load(fh)
 
 produkte=[]
-preis=[]
-datum=[]
+preise=[]
+datums=[]
 
 print("Daten\n")
 
 for eintrag in ausgaben:
     produkte.append(eintrag["produkt"])
-    preis.append(eintrag["preis"])
-    datum.append(eintrag["datum"])
+    preise.append(eintrag["preis"])
+    datums.append(eintrag["datum"])
 
 #Einträge eintragen
 for i in range (len(produkte)):
-    table.insert(parent="", index=i, values =(produkte[i], preis[i], datum[i])) 
+    table.insert(parent="", index=i, values =(produkte[i], preise[i], datums[i])) 
 
 # Run the application
 app.mainloop()
