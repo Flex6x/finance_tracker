c92d37b065e452b3804ccf6c464e634a62fdf266
Flex6x <hanf.felix@outlook.de>
2026-01-26 18:12:37 +0100

    monat wird in test datei ausgelesen

--- test_monat_auslesen.py
+++ test_monat_auslesen.py
@@ -11,4 +11,20 @@ for eintrag in ausgaben:
     preise.append(eintrag["preis"])
     datums.append(eintrag["datum"]) 
 
-print (preise)
+monat=[]
+
+
+
+for i in range (len(preise)):
+    z3 = str(datums[i])[4]
+    z4 = str(datums[i])[5]
+    z=z3+z4
+    monat.append(z)
+print(monat)
+
+
+#for i in range (len(preise)):
+    #z3 = (datums[i] // 1000) % 10
+    #z4 = (datums[i] // 100) % 10
+    #monat.append(z3,z4)
+    #print(monat)
